2cfe03d8aac85cdbfee5458e5250b2d29c264ace
Yassine Moussafir <yassinemoussafir31@gmail.com>
2024-09-03 11:36:33 +0100

    Index.py and App.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -8,18 +8,19 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
 
-
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+@app.errorhandler(401)
+def unauthorized_error(error):
+    """ Error handler for 401 Unauthorized """
+    return jsonify({"error": "Unauthorized"}), 401
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler
-    """
+    """ Not found handler """
     return jsonify({"error": "Not found"}), 404
-
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
